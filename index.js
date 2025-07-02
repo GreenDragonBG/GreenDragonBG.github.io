4abf41c7292d16ccfbe590bbd69690f879e42fa6
IvanK <ivan_koyrushki@abv.bg>
2025-07-02 10:35:00 +0200

    image scrpit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world, arring live from Ivan Koyrushki");
+console.log({"activity":"Make a simple musical instrument","availability":0.25,"type":"music","participants":1,"price":0.4,"accessibility":"Minor challenges","duration":"minutes","kidFriendly":true,"link":"","key":"7091374"});
+async function GetAPIData() {
+            try {
+                const response = await fetch("https://dog.ceo/api/breeds/image/random");
+                if (response.ok) {
+                    const data = await response.json();
+                	document.getElementById("image").src = data["message"];
+                } else {
+                    console.error(`HTTP error ${response.status}`);
+                }
+            } catch (error) {
+                console.error("Fetch failed:", error);
+            }
+        }
+ 
+		GetAPIData();
