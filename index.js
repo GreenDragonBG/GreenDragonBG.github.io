9724ef7ccda2e0dbcff45c42b695665a1cded349
IvanK <ivan_koyrushki@abv.bg>
2025-07-02 11:01:07 +0200

    five dogs

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 console.log({"activity":"Make a simple musical instrument","availability":0.25,"type":"music","participants":1,"price":0.4,"accessibility":"Minor challenges","duration":"minutes","kidFriendly":true,"link":"","key":"7091374"});
-async function GetAPIData() {
-            try {
+async function GetAPI() {
+            for(i =1;i<=5;i++){
+                try {
                 const response = await fetch("https://dog.ceo/api/breeds/image/random");
                 if (response.ok) {
                     const data = await response.json();
-                	document.getElementById("image").src = data["message"];
+                    const imageID = "image" + i;
+                	document.getElementById(imageID).src = data["message"];
                 } else {
                     console.error(`HTTP error ${response.status}`);
                 }
             } catch (error) {
                 console.error("Fetch failed:", error);
             }
+            }
         }
  
-		GetAPIData();
+		GetAPI();
